bef8e23d37b28643b16bb1fb91f116b054fa00fa
Sebastian Mohr <sebastianmohr96@gmail.com>
2024-03-31 16:50:44 +0100

    Add hypr scripts from @JaKooLit/Hyprland-Dots repository

--- hypr/scripts/BrightnessKbd.sh
+++ hypr/scripts/BrightnessKbd.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 # /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
-# Script for Monitor backlights (if supported) using brightnessctl
+# Script for keyboard backlights (if supported) using brightnessctl
 
 iDIR="$HOME/.config/swaync/icons"
-notification_timeout=1000
 
-# Get brightness
-get_backlight() {
-	echo $(brightnessctl -m | cut -d, -f4)
+# Get keyboard brightness
+get_kbd_backlight() {
+	echo $(brightnessctl -d '*::kbd_backlight' -m | cut -d, -f4)
 }
 
 # Get icons
 get_icon() {
-	current=$(get_backlight | sed 's/%//')
+	current=$(get_kbd_backlight | sed 's/%//')
 	if   [ "$current" -le "20" ]; then
 		icon="$iDIR/brightness-20.png"
 	elif [ "$current" -le "40" ]; then
@@ -25,29 +24,28 @@ get_icon() {
 		icon="$iDIR/brightness-100.png"
 	fi
 }
-
 # Notify
 notify_user() {
-	notify-send -e -h string:x-canonical-private-synchronous:brightness_notif -h int:value:$current -u low -i "$icon" "Brightness : $current%"
+	notify-send -e -h string:x-canonical-private-synchronous:brightness_notif -h int:value:$current -u low -i "$icon" "Keyboard Brightness : $current%"
 }
 
 # Change brightness
-change_backlight() {
-	brightnessctl set "$1" && get_icon && notify_user
+change_kbd_backlight() {
+	brightnessctl -d *::kbd_backlight set "$1" && get_icon && notify_user
 }
 
 # Execute accordingly
 case "$1" in
 	"--get")
-		get_backlight
+		get_kbd_backlight
 		;;
 	"--inc")
-		change_backlight "+10%"
+		change_kbd_backlight "+30%"
 		;;
 	"--dec")
-		change_backlight "10%-"
+		change_kbd_backlight "30%-"
 		;;
 	*)
-		get_backlight
+		get_kbd_backlight
 		;;
 esac
